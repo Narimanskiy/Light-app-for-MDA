904cad75c5f17a7390d54cd485daf9c8bb74984f
Nariman Narimanov <Narimanov7@yandex.ru>
2020-10-03 21:27:11 +0300

    Added button and make 3 color

--- Light app/ViewController.swift
+++ Light app/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    let colorsMaxldx = 2
+    let redColor = "red"
+    let yellowColor = "yellow"
+    let greenColor = "green"
+
+    var colordx = 0
+    
+    @IBAction func buttonPresed(_ sender: Any) {
+    colordx += 1
+        if colordx > colorsMaxldx {
+            colordx = 0
+        }
+        switch colordx {
+        case 0: view.backgroundColor = .red
+            break
+        case 1: view.backgroundColor = .yellow
+            break
+        default:
+            view.backgroundColor = .green
+        }
+    
+        func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
 }
 
+}
